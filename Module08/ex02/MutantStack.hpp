1e0a3a2b1e76bd682e5f6762c03e80ef78d3f683
Davide Cologgi <dcologgi@e3r2p8.42roma.it>
2024-01-17 10:43:02 +0100

    Module08 complete

--- Module08/ex02/MutantStack.hpp
+++ Module08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: dcologgi <dcologgi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 15:53:42 by dcologgi          #+#    #+#             */
-/*   Updated: 2024/01/16 15:57:38 by dcologgi         ###   ########.fr       */
+/*   Updated: 2024/01/17 10:01:16 by dcologgi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,44 @@
 # define MUTANTSTACK_HPP
 
 # include <iostream>
+# include <stack>
 
-class MutantStack {
-    
+template <typename T>
+class MutantStack : public std::stack<T> {
+    public:
+        class iterator {
+            public:
+                explicit iterator(typename std::stack<T>::container_type::iterator it) : current(it) {}
+
+                bool        operator!=(const iterator& other) const {
+                    return (current != other.current);
+                }
+
+                T&          operator*() const {
+                    return *current;
+                }
+
+                iterator&   operator++() {
+                    ++current;
+                    return *this;
+                }
+
+                iterator&   operator--() {
+                    --current;
+                    return *this;
+                }
+
+            private:
+                typename std::stack<T>::container_type::iterator current;
+        };
+
+        iterator    begin() {
+            return iterator(this->c.begin());
+        }
+
+        iterator    end() {
+            return iterator(this->c.end());
+        }
 };
 
 #endif
